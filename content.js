fe9f3b5071996566e929219f27d3fc5479be3456
Stilleur <stilleurfr@gmail.com>
2022-08-16 18:36:00 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,54 @@
+(() => {
+  debugger
+
+  const nextDataElement = document.getElementById('__NEXT_DATA__')
+  if (!nextDataElement) return
+
+  const jsonString = nextDataElement.textContent
+  const json = JSON.parse(jsonString)
+
+  switch (json.page) {
+    case '/SearchListing':
+      handleListing(json.props.pageProps.searchData.ads)
+      break
+    case '/ClassifiedAd':
+      handleAd(json.props.pageProps.ad)
+      break
+  }
+})()
+
+function handleListing(ads) {
+  const selectors = {
+    ad: 'a[data-qa-id=aditem_container]',
+    content: 'div[data-test-id=bigpicture-housing-content]',
+  }
+
+  const adEls = document.querySelectorAll(selectors.ad)
+  for (const adEl of adEls) {
+    const href = adEl.getAttribute('href')
+    const lastSlashIndex = href.lastIndexOf('/')
+    const endIndex = href.length - 4 // take out '.htm'
+
+    const id = href.substring(lastSlashIndex + 1, endIndex)
+    const ad = ads.find(ad => ad.list_id === Number(id))
+    const contentEl = adEl.querySelector(selectors.content)
+    const dateEl = contentEl.nextSibling.firstChild
+
+    dateEl.textContent = getDateText(ad.first_publication_date)
+  }
+}
+
+function handleAd(ad) {
+  const selectors = {
+    date: 'p[data-qa-id=adview_date]',
+  }
+
+  const el = document.querySelector(selectors.date)
+
+  el.textContent = getDateText(ad.first_publication_date)
+}
+
+function getDateText(date) {
+  return `Publié le ${date.toLocaleDateString('fr-FR')}`
+}
+
