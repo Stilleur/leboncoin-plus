0be8f27a25421f5885ae844529204f9afa8df9c7
Stilleur <stilleurfr@gmail.com>
2022-08-19 14:36:09 +0200

    chore: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin")
+const CleanPlugin = require('clean-webpack-plugin').CleanWebpackPlugin
+
+var options = {
+  mode: 'production',
+  entry: {
+    'background': './src/background.js',
+    'leboncoin-plus': './src/leboncoin-plus.js',
+  },
+  target: 'web',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './src/manifest.json' },
+      ],
+    }),
+    new CleanPlugin(),
+  ],
+}
+
+module.exports = options;
